31a704978187cfdb8e3d7e04dcd80c6a6379f194
dbrider05 <abdqavi39@gmail.com>
2021-09-08 14:32:33 +0530

    initial commit

---
+++ Patterns/butterfly.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    int n, i, j, spaces;
+    cout<<"input n:";
+    cin>>n;
+
+    for(i=1;i<=n;i++){
+        for(j=1;j<=i;j++)
+            cout<<"*";
+        
+        spaces = 2*n - 2*i;
+        
+        for(j=1;j<=spaces;j++)
+            cout<<" ";
+        
+        for(j=1;j<=i;j++)
+            cout<<"*";
+        
+        cout<<endl;
+    }
+
+    for(i=n;i>=1;i--){
+        for(j=1;j<=i;j++)
+            cout<<"*";
+        
+        spaces = 2*n - 2*i;
+        
+        for(j=1;j<=spaces;j++)
+            cout<<" ";
+        
+        for(j=1;j<=i;j++)
+            cout<<"*";
+        
+        cout<<endl;
+    }
+    return 0;
+} 
